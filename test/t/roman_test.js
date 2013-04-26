caac009a456fb16555ade39aaba5b0789c562b3a
hankei6km <hankei6km@gmail.com>
2013-04-26 14:11:39 +0900

    ローマ字変換用のライブラリ追加

---
+++ test/t/roman_test.js
@@ -0,0 +1,42 @@
+/**
+ * @file ローマ字変換のテスト.
+ * @author hankei6km
+ * @copyright (c) 2013 hankei6km
+ * @license MIT License (http://opensource.org/licenses/mit-license.php)
+ */
+"use strict";
+
+var helper=require('../test_helper.js');
+var QUnit=helper.QUnit;
+var assert=helper.assert;
+var path = require('path');
+var util = require('util');
+
+QUnit.module('roman');
+
+var roman = require('../../lib/roman');
+
+
+QUnit.test('conv', function() {
+  assert.strictEqual(roman.conv('aiueo'), 'あいうえお', 'aiueo');
+  assert.strictEqual(roman.conv('kakikukeko'), 'かきくけこ', 'kakikukeko');
+  assert.strictEqual(roman.conv('gyouza'), 'ぎょうざ', 'gyouza');
+  assert.strictEqual(roman.conv('kare-'), 'かれー', 'kare-');
+  assert.strictEqual(roman.conv('kare-soba'), 'かれーそば', 'kare-soba');
+  assert.strictEqual(roman.conv('rakkyou'), 'らっきょう', 'rakkyou');
+  assert.strictEqual(roman.conv('raltukyou'), 'らっきょう', 'raltukyou');
+  assert.strictEqual(roman.conv('raltukilyou'), 'らっきょう', 'raltukilyou');
+  assert.strictEqual(roman.conv('anpan'), 'あんぱｎ', 'anpan');
+  assert.strictEqual(roman.conv('annpan'), 'あんぱｎ', 'anmpan');
+  assert.strictEqual(roman.conv('annpann'), 'あんぱん', 'anmpan');
+  assert.strictEqual(roman.conv('nn'), 'ん', 'nn');
+});
+
+QUnit.test('not roman', function() {
+  assert.strictEqual(roman.conv('[]'), '「」', '[]');
+  assert.strictEqual(roman.conv('()'), '（）', '()');
+  assert.strictEqual(roman.conv(',.'), '、。', ',.');
+  assert.strictEqual(roman.conv('/'), '・', '/');
+  assert.strictEqual(roman.conv('!"#$%&'), '！”＃＄％＆', '!"#$%&');
+  assert.strictEqual(roman.conv('qwe'), 'ｑうぇ', 'qwe');
+});
