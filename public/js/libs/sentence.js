2edb813835d20d1db77d6d13725827a573e2a0f1
hankei6km <hankei6km@gmail.com>
2013-05-09 20:13:54 +0900

    通常変換による入力操作追加

--- public/js/libs/sentence.js
+++ public/js/libs/sentence.js
@@ -16,9 +16,10 @@ var Sentence = function($textArea, contextMenuSelector){
                       function(e){ 
                         that.focusedContextMenuItem = e.target.textContent;
                         if(that.isPreEdit()){
-                          that.insFld(that.$target, that.focusedContextMenuItem, that.insPos,
-                                      that.insPos + that.preLen);
-                          that.preLen = that.focusedContextMenuItem.length;
+                          var s = that.getSeledTxt(that.focusedContextMenuItem);
+                          that.insFld(that.$target, s.txt, that.insPos,
+                                      that.insPos + that.preLen, s.dlt);
+                          that.preLen = s.txt.length;
                         }
                       });
   $(document.body).on("contextmenu:blur", ".context-menu-item",
@@ -35,13 +36,13 @@ var Sentence = function($textArea, contextMenuSelector){
         }else{
           that.hiragana = that.hiragana.substr(0, that.hiragana.length-1);
         }
-        that.get($target);
+        that.predictive($target);
         event.preventDefault();
       }
     }else if(event.keyCode == 27){
       if(that.isPreEdit()){
         if(that.focusedContextMenuItem){
-          that.get($target);
+          that.predictive($target);
         }else{
           that.insFld($target, '', that.insPos, that.insPos + that.preLen);
           that.initProp();
@@ -66,23 +67,46 @@ var Sentence = function($textArea, contextMenuSelector){
         that.status = !that.status;
       }
       event.preventDefault();
+    }else if(event.keyCode == 32){
+      if(that.isPreEdit()){
+        if(that.popuped){
+          that.$menu.trigger('nextcommand');
+        }else{
+          that.normal($target);
+        }
+        event.preventDefault();
+      }
+    }else if(event.keyCode == 37){
+      if(that.isPreEdit() && that.segmentsInfo){
+        that.switchSegment(that.$target, -1);
+        event.preventDefault();
+      }
+    }else if(event.keyCode == 39){
+      if(that.isPreEdit() && that.segmentsInfo){
+        that.switchSegment(that.$target, 1);
+        event.preventDefault();
+      }
     }
   });
   $textArea.keypress(function(event){
     if(that.status){
       var chr = String.fromCharCode(event.which);
       var $target = $(event.target);
-      var code = event.keyCode != 0 ? event.keyCode: event.charCode;
-      if ((32 <= code && code < 127)) {
+      //var code = event.keyCode != 0 ? event.keyCode: event.which;
+      var code = event.which
+      if ((32 < code && code < 127)) {
         if(that.focusedContextMenuItem || that.katakana){
           // 選択中の候補があったので、確定する.
+          var pos = that.insPos + that.preLen;
+          $target.prop("selectionStart", pos);
+          $target.prop("selectionEnd", pos);
           that.initProp();
         }
         if(!that.isPreEdit()){
           that.insPos = $target.prop("selectionStart");
         }
         that.inputText = that.inputText + chr.toLocaleLowerCase();
-        that.get($target);
+        that.predictive($target);
         event.preventDefault();
       }
     }
@@ -94,17 +118,57 @@ var Sentence = function($textArea, contextMenuSelector){
 Sentence.prototype.initProp = function(){
   this.inputText = '';
   this.hiragana = '';
+  this.segmentsInfo = null;
   $.contextMenu( 'destroy', this.contextMenuSelector);
+  this.popuped = false;
   this.$target = null;
   this.katakana = false;
   this.preLen = 0;
+}
+
+Sentence.prototype.setSegmentsInfo = function(segments){
+  var len = segments.length;
+  var sels = new Array(len);
+  for(var idx=0; idx<len; idx++){
+    sels[idx] = segments[idx].candidates[0];
+  }
+
+  this.segmentsInfo = {
+    curpos: 0,
+    segments: segments,
+    sels: sels
+  };
+}
+
+Sentence.prototype.getSeledTxt = function(in_txt, in_pos){
+  var ret = {
+    txt: in_txt,
+    dlt: null
+  }
+  if(this.segmentsInfo){
+    ret.txt = '';
+    var pos = typeof(in_pos)!='undefined'? in_pos: this.segmentsInfo.curpos;
+    if(typeof(in_txt)!='undefined'){
+      this.segmentsInfo.sels[pos] = in_txt;
+    }
+    var len = this.segmentsInfo.segments.length;
+    for(var idx=0; idx<len; idx++){
+      ret.txt = ret.txt + this.segmentsInfo.sels[idx];
+    }
+    ret.dlt = 0;
+    for(var idx=0; idx<=pos; idx++){
+      ret.dlt = ret.dlt + this.segmentsInfo.sels[idx].length;
+    }
+
+  }
+  return ret;
 }
 
 Sentence.prototype.isPreEdit = function(){
   return !(this.inputText === '' && this.hiragana === '');
 };
 
-Sentence.prototype.get = function($target){
+Sentence.prototype.predictive = function($target){
   var that = this;
   this.$target = $target;
 
@@ -121,42 +185,36 @@ Sentence.prototype.get = function($target){
   this.insFld($target, sentence, this.insPos, this.insPos + this.preLen);
   this.preLen = sentence.length;
 
-  this.conv.get('predictive', sentence, function(err, resp){
+  this.get('predictive', sentence, $target);
+  this.popuped = false;
+};
+
+Sentence.prototype.normal = function($target){
+  var sentence = this.hiragana;
+  this.get('normal', sentence, $target);
+  this.popuped = true;
+};
+
+Sentence.prototype.get = function(mode, sentence, $target){
+  var that = this;
+  this.segmentsInfo = null;
+  this.conv.get(mode, sentence, function(err, resp){
     if(!err){
       // エラーになるがとりあえず動くのでそのまま.
       $.contextMenu( 'destroy',  that.contextMenuSelector);
 
       if(sentence){
+        if(mode == 'normal'){
+          that.setSegmentsInfo(resp.segments);
+        }
         var candidates = resp.segments[0].candidates;
         var len = candidates.length; 
         if(len > 0){
-          // 推測変換候補をitemsにする.
-          var items = {};
-          for(var idx=0; idx<len; idx++){
-            var word = candidates[idx];
-            items[word] = {name: _.escape(word)};
-          }
-
-          $.contextMenu({
-            selector: that.contextMenuSelector,
-            trigger: 'none',
-            position: function(opt, x, y){
-              var $win = $(window);
-              var bottom = $win.scrollTop() + $win.height();
-              var height = opt.$menu.height()
-              if(y + height > bottom){
-                y = bottom - height 
-                if(y < 0){
-                  y = 0;
-                }
-                x = x + 8; // ここの値は本来ならフォントサイズから求めるべきだが
-              }
-              opt.$menu.css({top: y, left: x});
-            },
-            callback: function(key, options) {
-              that.insFld($target, key, that.insPos,  that.insPos + that.preLen);
-            },
-            items: items
+          that.setPopupItem(mode, $target, candidates, function(opt){
+            if(mode == 'normal'){
+              // 通常の変換ならば、最初の項目を選択しておく.
+              opt.$menu.trigger('nextcommand');
+            }
           });
 
           var cp = Measurement.caretPos($target);
@@ -167,11 +225,84 @@ Sentence.prototype.get = function($target){
         }
       }
     }
-
   });
 };
 
-Sentence.prototype.insFld = function($target, insTxt, insStartPos, insEndPos){
+Sentence.prototype.setPopupItem = function(mode, $target, candidates, cb){
+  var len = candidates.length; 
+
+  // 推測変換候補をitemsにする.
+  var items = {};
+  for(var idx=0; idx<len; idx++){
+    var word = candidates[idx];
+    items[word] = {name: _.escape(word)};
+  }
+
+  var that = this;
+  $.contextMenu({
+    selector: that.contextMenuSelector,
+    trigger: 'none',
+    position: function(opt, x, y){
+      var $win = $(window);
+      var bottom = $win.scrollTop() + $win.height();
+      var height = opt.$menu.height()
+      if(y + height > bottom){
+        y = bottom - height 
+        if(y < 0){
+          y = 0;
+        }
+        x = x + 8; // ここの値は本来ならフォントサイズから求めるべきだが
+      }
+      opt.$menu.css({top: y, left: x});
+      if(cb){
+        cb(opt);
+      }
+      that.$menu = opt.$menu;
+    },
+    callback: function(key, options) {
+      that.insFld($target, that.getSeledTxt(key).txt,
+                  that.insPos,  that.insPos + that.preLen);
+    },
+    items: items
+  });
+};
+
+Sentence.prototype.switchSegment = function($target, allow){
+  var refresh = false;
+  if(allow < 0){
+    if(this.segmentsInfo.curpos > 0){
+      this.segmentsInfo.curpos--;
+      refresh = true;
+    }
+  }else{
+    if(this.segmentsInfo.curpos < this.segmentsInfo.segments.length - 1){
+      this.segmentsInfo.curpos++;
+      refresh = true;
+    }
+  }
+  if(refresh){
+    var curpos = this.segmentsInfo.curpos;
+    $.contextMenu( 'destroy',  this.contextMenuSelector);
+    var candidates = this.segmentsInfo.segments[curpos].candidates;
+    var that = this;
+    this.setPopupItem('normal', $target, 
+                     candidates,
+                     function(opt){
+                       var i = candidates.indexOf(that.segmentsInfo.sels[curpos]);
+                       for(var idx=0; idx<=i; idx++){
+                         opt.$menu.trigger('nextcommand');
+                       }
+                     });
+
+    var cp = Measurement.caretPos($target);
+    $(this.contextMenuSelector).contextMenu({
+      x: cp.left,
+      y: cp.top + 18  // ここの値は本来ならフォントサイズから求めるべきだが
+    });
+  }
+};
+
+Sentence.prototype.insFld = function($target, insTxt, insStartPos, insEndPos, dltPos){
   var startPos = insStartPos;
   var endPos = insEndPos;
   if(typeof(insStartPos) == 'undefined'){
@@ -185,6 +316,9 @@ Sentence.prototype.insFld = function($target, insTxt, insStartPos, insEndPos){
   var s = t.substring(endPos);
   $target.val(p + insTxt+ s);
   var pos = startPos + insTxt.length;
+  if(typeof(dltPos) == 'number'){
+    pos = startPos + dltPos;
+  }
   $target.prop("selectionStart", pos);
   $target.prop("selectionEnd", pos);
 }
