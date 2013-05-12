2c167a3e20990b2c0d52445e5cc901c4e723f004
hankei6km <hankei6km@gmail.com>
2013-05-12 20:39:07 +0900

    node-zinniaのapiをコールするときにtry-catch

--- apps/socket_hwr.js
+++ apps/socket_hwr.js
@@ -25,31 +25,47 @@ module.exports = function(server){
     var sidx = -1;
 
     socket.on('clear', function (data) {
-      s.set_width(data.width);
-      s.set_height(data.height);
-      s.clear();
-      sidx = -1;
+      try{
+        s.set_width(data.width);
+        s.set_height(data.height);
+        s.clear();
+        sidx = -1;
+      }catch(e){
+        sendErr(e)
+      }
     });
     socket.on('add', function (data) {
       if(sidx+1 == data.idx){
-        var len = data.pt.length;
-        for(var idx=0; idx<len; idx++){
-          var pt = data.pt[idx];
-          s.add(data.idx, pt.x, pt.y);
-        }
-        r.classify(s, data.nbest,function(result){
-          var size = result.size();
-          var v = new Array(size);
-          for(var i=0; i<size; i++){
-            v[i] = {value:result.value(i),socre:result.score(i)};
+        try{
+          var len = data.pt.length;
+          for(var idx=0; idx<len; idx++){
+            var pt = data.pt[idx];
+            s.add(data.idx, pt.x, pt.y);
           }
-          socket.emit('result', {idx: data.idx, results:v});
-        });
-        sidx++;
+          r.classify(s, data.nbest,function(result){
+            var size = result.size();
+            var v = new Array(size);
+            for(var i=0; i<size; i++){
+              v[i] = {value:result.value(i),socre:result.score(i)};
+            }
+            socket.emit('result', {idx: data.idx, results:v});
+          });
+          sidx++;
+        }catch(e){
+          sendErr(e)
+        }
       }else{
-        socket.emit('error', {message: 'invalid idx = ' + data.idx});
+        sendErr('invalid idx = ' + data.idx);
       }
     });
+    var sendErr = function(in_message){
+      var message = in_message;
+      if(typeof(in_message) != 'stringr'){
+        message = in_message.toString();
+      }
+      socket.emit('error', {message: message});
+    };
+
   });
 
 };
